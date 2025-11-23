5955c39958ee4ec553c99e13598507ee537f6c79
Muhammad Ibraheem <muhammadibraheem177@gmail.com>
2025-11-23 18:44:36 +0500

    convert home and checkout to async await

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -8,10 +8,11 @@ export function HomePage({cart}) {
     
     
     useEffect(() => {
-        axios.get('/api/products')
-            .then((response) => {
+        const getHomeData = async ()=>{
+            const response = await axios.get('/api/products')
                 setProducts(response.data)
-            });
+        }
+        getHomeData();
         
     }, [])
 
