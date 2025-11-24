4569566327708d422a1fcf3ebad75db14755e57a
Muhammad Ibraheem <muhammadibraheem177@gmail.com>
2025-11-24 14:47:49 +0500

    create integration test for product component

---
+++ src/Pages/Home/Product.test.jsx
@@ -0,0 +1,45 @@
+import { it, expect, describe, vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import { Product } from './Product'
+
+describe('product component', () => {
+    it('displays the product details correctly', () => {
+
+        const product =   {
+    id: "1c079479-8586-494f-ab53-219325432536",
+    image: "images/products/men-athletic-shoes-white.jpg",
+    name: "Men's Athletic Sneaker - White",
+    rating: {
+      stars: 4,
+      count: 229
+    },
+    priceCents: 4590,
+    keywords: ["shoes", "running shoes", "footwear", "mens"]
+  }
+
+        const loadCart = vi.fn()
+
+        render(<Product product={product} loadCart={loadCart} />)
+
+        expect(
+            screen.getByText("Men's Athletic Sneaker - White")
+        ).toBeInTheDocument()
+
+        expect(
+            screen.getByText('$45.90')
+        ).toBeInTheDocument()
+
+        expect(
+            screen.getByTestId('product-image')
+        ).toHaveAttribute('src', 'images/products/men-athletic-shoes-white.jpg')
+
+        expect(
+            screen.getByTestId('product-rating-stars-image')
+        ).toHaveAttribute('src', 'images/ratings/rating-40.png')
+
+        expect(
+            screen.getByText('229')
+        ).toBeInTheDocument()
+
+    })
+})
